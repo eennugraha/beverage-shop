b8ba6d713f54cf31218368101a83f054a6ef1a74
eennugraha <nugraha.hendrisatya@gmail.com>
2022-08-15 08:22:51 +0700

    tes env

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-// app.listen(port, () => {
-//   console.log(`App is listening on ${port}`);
-// });
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
 
 const routes = require("./routes");
 app.use(routes);
